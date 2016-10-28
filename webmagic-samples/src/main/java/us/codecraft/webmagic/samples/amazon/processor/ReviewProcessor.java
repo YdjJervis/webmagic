4343f23976b0c0b50f66a6326b75f713b4668ad1
Jervis <ydj_jervis@foxmail.com>
2016-10-28 11:09:18 +0800

    修复Review解析

--- webmagic-samples/src/main/java/us/codecraft/webmagic/samples/amazon/processor/ReviewProcessor.java
+++ webmagic-samples/src/main/java/us/codecraft/webmagic/samples/amazon/processor/ReviewProcessor.java
@@ -85,9 +85,10 @@ public class ReviewProcessor extends BasePageProcessor implements ScheduledTask 
         Review review = new Review();
         int star = Integer.valueOf(reviewNode.xpath("//*[@data-hook='review-star-rating']/@class").regex(".*-([0-5]).*").get());//提取星级
         String title = reviewNode.xpath("//a[@class='a-size-base a-link-normal review-title a-color-base a-text-bold']/text()").get();
-        String reviewId = reviewNode.xpath("//a[@class='a-size-base a-link-normal review-title a-color-base a-text-bold']/@href").regex(".*customer-reviews/(.*)/ref.*").get();
-        String person = reviewNode.xpath("//a[@class='a-size-base a-link-normal author']/text()").get();
-        String personID = reviewNode.xpath("//a[@class='a-size-base a-link-normal author']/@href").regex(".*profile/(.*)/ref.*").get();
+//        String reviewId = reviewNode.xpath("//a[@data-hook='review-title']/@href").regex(".*customer-reviews/(.*)/ref.*").get();
+        String reviewId = reviewNode.xpath("div/@id").get();
+        String person = reviewNode.xpath("//a[@data-hook='review-author']/text()").get();
+        String personID = reviewNode.xpath("//a[@data-hook='review-author']/@href").regex(".*profile/(.*)/ref.*").get();
         String time = reviewNode.xpath("//span[@class='a-size-base a-color-secondary review-date']/text()").get();
         String version = reviewNode.xpath("//a[@class='a-size-mini a-link-normal a-color-secondary]/text()").get();
         String content = reviewNode.xpath("//span[@class='a-size-base review-text]/text()").get();
