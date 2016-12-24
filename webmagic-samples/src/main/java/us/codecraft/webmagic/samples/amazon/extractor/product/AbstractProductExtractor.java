9845e92ed20a8a1abfdef1ceb76826b19df0221c
Jervis <ydj_jervis@foxmail.com>
2016-12-24 18:27:19 +0800

    不同站点产品抽取器抽取框架搭建

---
+++ webmagic-samples/src/main/java/us/codecraft/webmagic/samples/amazon/extractor/product/AbstractProductExtractor.java
@@ -0,0 +1,23 @@
+package us.codecraft.webmagic.samples.amazon.extractor.product;
+
+import org.apache.log4j.Logger;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.samples.amazon.pojo.Product;
+
+/**
+ * @author Jervis
+ * @version V0.2
+ * @Description: 产品抽取器抽象类，做一些公共操作
+ * @date 2016/12/24 18:12
+ */
+public abstract class AbstractProductExtractor implements ProductExtractor{
+
+    Logger sLogger = Logger.getLogger(getClass());
+    protected Product sProduct;
+
+    @Override
+    public Product extract(String asin, Page page) {
+        sProduct = new Product();
+        return sProduct;
+    }
+}
