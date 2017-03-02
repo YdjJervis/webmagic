68a35d6a4071d3b3f3867de93fe74b3c6c21176a
Jervis <ydj_jervis@foxmail.com>
2017-03-02 16:00:40 +0800

    修复：缓存实现类有Bug，换了一个开源的解决方案

--- webmagic-samples/src/main/javaspider/com/eccang/spider/amazon/service/UrlService.java
+++ webmagic-samples/src/main/javaspider/com/eccang/spider/amazon/service/UrlService.java
@@ -1,14 +1,9 @@
 package com.eccang.spider.amazon.service;
 
-import com.google.gson.Gson;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import com.eccang.spider.amazon.R;
 import com.eccang.spider.amazon.dao.UrlDao;
-import com.eccang.spider.amazon.pojo.*;
+import com.eccang.spider.amazon.pojo.Asin;
+import com.eccang.spider.amazon.pojo.Url;
 import com.eccang.spider.amazon.pojo.batch.Batch;
 import com.eccang.spider.amazon.pojo.batch.BatchAsin;
 import com.eccang.spider.amazon.pojo.batch.BatchAsinExtra;
@@ -19,6 +14,11 @@ import com.eccang.spider.amazon.service.crawl.ReviewService;
 import com.eccang.spider.amazon.service.relation.AsinRootAsinService;
 import com.eccang.spider.amazon.service.relation.CustomerAsinService;
 import com.eccang.spider.base.util.UrlUtils;
+import com.google.gson.Gson;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.*;
 
@@ -87,7 +87,7 @@ public class UrlService {
 
     public void addAll(List<Url> urlList) {
 
-        List<Url> newList = new ArrayList<>();
+        /*List<Url> newList = new ArrayList<>();
 
         for (Url url : urlList) {
             if (!isExist(url.urlMD5)) {
@@ -99,7 +99,8 @@ public class UrlService {
 
         if (CollectionUtils.isNotEmpty(newList)) {
             mUrlDao.addAll(newList);
-        }
+        }*/
+        mUrlDao.addAll(urlList);
 
     }
 
